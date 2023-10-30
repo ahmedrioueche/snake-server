e844edb9946ff8d3f323e6a8b6cbf0c2d8bbb532
ahmedrioueche <116762968+ahmedrioueche@users.noreply.github.com>
2023-10-30 03:36:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://snake-blush.vercel.app/", 
+    origin: "https://snake-blush.vercel.app", 
     methods: ["GET", "POST"],
   },
 });
